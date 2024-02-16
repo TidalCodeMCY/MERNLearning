5638fe7ee77998a29201896be913e4ceacaf73a9
Mason Yates <Tidalabysstv@gmail.com>
2024-02-16 13:34:20 -0600

    started creation of frontend

--- MernPractice/backend/index.js
+++ MernPractice/backend/index.js
@@ -2,120 +2,32 @@ import express, { request } from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/bookRoutes.js";
+import cors from 'cors';
 
 const app = express()
 
 // Middleware for parsin request body.
 app.use(express.json());
 
+// Middleware for CORS POLICY
+// Option 1: Allow all origins with default of cors(*) - This is not recommended to use unless necessary!
+// app.use(cors());
+// Option 2: Allow custom origins - better for security reasons I believe
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/',(request,response) => {
     console.log(request);
     return response.status(234).send('Welcome to the Mern stack tutorial!');
 });
 
-// Route to save a new book
-app.post('/books', async (request, response) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all required fields: title,author,publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        }
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-})
-
-// Route to get all books from the database
-app.get('/books', async (request, response) => {
-    try{
-        const books = await Book.find({});
-
-        return response.status(200).json({
-           count: books.length,
-           data: books
-        });
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route to get one book from the database by id
-app.get('/books/:id', async (request, response) => {
-    try{
-        const{ id } = request.params;
-
-        const books = await Book.findById(id);
-
-        return response.status(200).json(books);
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route to update the book
-app.put('/books/:id', async (request, response) => {
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all required fields: title,author,publishYear',
-            });
-        }
-
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if(!result){
-            return response.status(404).json({message: 'Book not found.'});
-        }
-
-        return response.status(200).send({message: 'Book updated successfully.'});
-
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route to delete book
-app.delete('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndDelete(id);
-
-        if(!result){
-            return response.status(404).json({ message: 'Book not found'})
-        }
-
-        return response.status(200).send({message: 'Book deletion successful!'})
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
---
+++ MernPractice/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import CreateBooks from './pages/CreateBooks.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import ShowBook from './pages/ShowBook.jsx';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+      <Route path='' element={}></Route>
+    </Routes>
+  )
+}
