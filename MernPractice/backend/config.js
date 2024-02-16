5b81dc6726ff6909e0653fe0f92629076367f09f
Mason Yates <Tidalabysstv@gmail.com>
2024-02-16 17:40:27 -0600

    finished more functionsality

--- MernPractice/backend/config.js
+++ MernPractice/backend/config.js
@@ -3,4 +3,4 @@ export const PORT = 5555;
 
 // The mongodb URL needed to access and use the database we created on mongo db
 export const mongoDBURL = 
-'mongodb+srv://root:<password>@books-store-mern.kuedadf.mongodb.net/?retryWrites=true&w=majority'
+'mongodb+srv://root:New831feat999!@books-store-mern.kuedadf.mongodb.net/?retryWrites=true&w=majority'
--- MernPractice/frontend/src/pages/CreateBooks.jsx
+++ MernPractice/frontend/src/pages/CreateBooks.jsx
@@ -36,7 +36,36 @@ export default function CreateBooks() {
       <BackButton />
       <h1 className="text-3x1 my-4">Create Book</h1>
       {loading ? <Spinner /> : ''}
-      
+      <div className="flex flex-col border-2 border-sky-400 rounded-x1 w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="text"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>Save</button>
+      </div>
     </div>
   )
 }
