61039515c1ea438b2910a81c019d6149d8d5419c
Mason Yates <Tidalabysstv@gmail.com>
2024-02-16 17:13:37 -0600

    fixed

--- MernPractice/frontend/src/pages/CreateBooks.jsx
+++ MernPractice/frontend/src/pages/CreateBooks.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useState } from "react";
+import React from 'react';
+import BackButton from '../components/BackButton.jsx';
+import Spinner from '../components/Spinner.jsx';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 export default function CreateBooks() {
+  const[title, setTitle] = useState('');
+  const[author, setAuthor] = useState('');
+  const[publishYear, setPublishYear] = useState('');
+  const[loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened please check console!');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>CreateBooks</div>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3x1 my-4">Create Book</h1>
+      {loading ? <Spinner /> : ''}
+      
+    </div>
   )
 }
